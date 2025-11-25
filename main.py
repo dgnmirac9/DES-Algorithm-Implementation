f9040ca45909e42df370ccccfe19eb751c35c5fa
Miraç Doğan <doganmirac612@gmail.com>
2025-11-25 21:45:19 +0300

    Main.py kodunda duzenlemeler yapildi

--- main.py
+++ main.py
@@ -2,31 +2,31 @@ from Crypto.Cipher import DES
 from Crypto.Util.Padding import pad
 import binascii
 
-#Fatih hoca son 2 hanesini şifrelememizi istedi.
+#OgrenciNoSon2Hane.
 ogrenciNumaram = '18' 
 
-#18'i byte formatına çevirdik.
+#18'i byte formatina cevirdik.
 data = ogrenciNumaram.encode('utf-8')
 
 #Anahtar belirleme
-key = b'MiracBey' 
+key = b'Rastgele' 
 #8 byte'lık anahtar 
 
-#DES algoritması veriyi 8 byte'lık gruplar halinde şifreler. 
-#Boşluğa yer yoktur. 18 -> 2 byte -> kalan 6 byte'ı da doldurmamız gerek.
-#Bunun için padding işlemine başvuracağız.
+#DES algoritmasi veriyi 8 byte'lik gruplar halinde sifreler. 
+#Bosluga yer yoktur. 18 -> 2 byte -> kalan 6 byte'i da doldurmamiz gerek.
+#Bunun icin padding islemine basvuracagiz.
 #block_size -> DES blok boyutu
 padded_data = pad(data, block_size=8)
 
 print(f"Orijinal Veri: {data}")
 print(f"Padding (Dolgu) yapilmis veri (hex): {binascii.hexlify(padded_data)}")
 
-#Şifreleme 
-#ECB -> Şifreleme modu
+#Sifreleme 
+#ECB -> Sifreleme modu
 cipher = DES.new(key, DES.MODE_ECB)
 sifreli_veri = cipher.encrypt(padded_data)
 
-#Sonuçlar
+#Sonuclar
 print("-" * 30)
 print(f"Kullanilan anahtar: {key}")
 print(f"Sifrelenmis veri (hex): {binascii.hexlify(sifreli_veri).decode('utf-8').upper()}")
